33c81b4f65092a45dec5597e0c5bc0c82a8d3ef6
Tanvi Narain <tanvi.narain@kellogg.northwestern.edu>
2021-02-22 20:13:16 -0600

    commit

--- homework/movies.js
+++ homework/movies.js
@@ -1,36 +1,55 @@
-window.addEventListener('DOMContentLoaded', async function(event) {
-  let db = firebase.firestore()
-  let apiKey = 'your TMDB API key'
-  let response = await fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}&language=en-US`)
+let db = firebase.firestore()
+firebase.auth().onAuthStateChanged(async function(user) {
+  let response = await fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=ebcd0c8bd7f9634f9dd933c786e737bb&language=en-US`)
   let json = await response.json()
   let movies = json.results
   console.log(movies)
-  
+  if (user) {
+  db.collection('users').doc(user.uid).set({
+    name: user.displayName,
+    email: user.email
+  })
   for (let i=0; i<movies.length; i++) {
     let movie = movies[i]
-    let docRef = await db.collection('watched').doc(`${movie.id}`).get()
+    let docRef = await db.collection('watched').doc(`${movie.id}-${user.id}`).get()
     let watchedMovie = docRef.data()
     let opacityClass = ''
     if (watchedMovie) {
       opacityClass = 'opacity-20'
     }
-
     document.querySelector('.movies').insertAdjacentHTML('beforeend', `
       <div class="w-1/5 p-4 movie-${movie.id} ${opacityClass}">
         <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" class="w-full">
         <a href="#" class="watched-button block text-center text-white bg-green-500 mt-4 px-4 py-2 rounded">I've watched this!</a>
       </div>
     `)
-
     document.querySelector(`.movie-${movie.id}`).addEventListener('click', async function(event) {
       event.preventDefault()
       let movieElement = document.querySelector(`.movie-${movie.id}`)
       movieElement.classList.add('opacity-20')
-      await db.collection('watched').doc(`${movie.id}`).set({})
+      await db.collection('watched').doc(`${movie.id}-${user.id}`).set({})
+    })
+  }
+    document.querySelector('.sign-in-or-sign-out').innerHTML = `
+      <button class="text-pink-500 underline sign-out">Sign Out</button>
+    `
+    document.querySelector('.sign-out').addEventListener('click', function(event) {
+      console.log('sign out clicked')
+      firebase.auth().signOut()
+      document.location.href = 'movies.html'
     }) 
+  } else {
+    let ui = new firebaseui.auth.AuthUI(firebase.auth())
+    let authUIConfig = {
+      signInOptions: [
+        firebase.auth.EmailAuthProvider.PROVIDER_ID
+      ],
+      signInSuccessUrl: 'movies.html'
+    }
+    ui.start('.sign-in-or-sign-out', authUIConfig)
   }
 })
-
+  
 // Goal:   Refactor the movies application from last week, so that it supports
 //         user login and each user can have their own watchlist.
 
